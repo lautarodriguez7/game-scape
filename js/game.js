3faef45d2625fddc9d81bed4cde264eea42d0373
Lautaro Rodriguez <lautarodriguez17@gmail.com>
2019-08-09 15:14:37 -0300

    new function "paint()"

--- js/game.js
+++ js/game.js
@@ -19,3 +19,11 @@ var mousex = 0,
         if (y > canvas.height)
             y = canvas.height;
     }
+
+    function paint() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.strokeStyle='#0f0';
+        ctx.beginPath();
+        ctx.arc(x, y, 5, 0, Math.PI*2, true);
+        ctx.strokeStyle();
+    }
