d70b705b12cf9486773668fe9927d8fc969343a9
Lautaro Rodriguez <lautarodriguez17@gmail.com>
2019-08-09 15:08:55 -0300

    added function act()

--- js/game.js
+++ js/game.js
@@ -1,2 +1,21 @@
 var mousex = 0,
     mousey = 0;
+
+    document.addEventListener('mousemove', function(evt) {
+        mousex = evt.pageX - canvas.offsetLeft;
+        mousey = evt.pageY - canvas.offsetTop;
+    }, false);
+
+    function act() {
+        x = mousex;
+        y = mousey;
+
+        if (x < 0) 
+            x = 0;
+        if (x > canvas.width)
+            x = canvas.width;
+        if (y < 0)
+            y = 0;
+        if (y > canvas.height)
+            y = canvas.height;
+    }
