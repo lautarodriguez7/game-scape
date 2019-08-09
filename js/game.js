7b0436d6bac4608413ba493cae588a5d6d4131eb
Lautaro Rodriguez <lautarodriguez17@gmail.com>
2019-08-09 15:21:17 -0300

    page game.js it is function local

--- js/game.js
+++ js/game.js
@@ -1,29 +1,33 @@
-var mousex = 0,
-    mousey = 0;
+(function() {
+    'use strict'
+    var mousex = 0,
+        mousey = 0;
 
-    document.addEventListener('mousemove', function(evt) {
-        mousex = evt.pageX - canvas.offsetLeft;
-        mousey = evt.pageY - canvas.offsetTop;
-    }, false);
+        document.addEventListener('mousemove', function(evt) {
+            mousex = evt.pageX - canvas.offsetLeft;
+            mousey = evt.pageY - canvas.offsetTop;
+        }, false);
 
-    function act() {
-        x = mousex;
-        y = mousey;
+        function act() {
+            x = mousex;
+            y = mousey;
 
-        if (x < 0) 
-            x = 0;
-        if (x > canvas.width)
-            x = canvas.width;
-        if (y < 0)
-            y = 0;
-        if (y > canvas.height)
-            y = canvas.height;
-    }
+            if (x < 0) 
+                x = 0;
+            if (x > canvas.width)
+                x = canvas.width;
+            if (y < 0)
+                y = 0;
+            if (y > canvas.height)
+                y = canvas.height;
+        }
 
-    function paint() {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.strokeStyle='#0f0';
-        ctx.beginPath();
-        ctx.arc(x, y, 5, 0, Math.PI*2, true);
-        ctx.strokeStyle();
-    }
+        function paint() {
+            ctx.art(centerX, centerY, radius, startAngle, endAngle, clockwise);
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            ctx.strokeStyle='#0f0';
+            ctx.beginPath();
+            ctx.arc(x, y, 5, 0, Math.PI*2, true);
+            ctx.strokeStyle();
+        }
+})();
